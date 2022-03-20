405ee6395718e08937fd7406591fbd34af1b1b29
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-03-20 12:52:43 +0100

    Removed unnecessary imports

--- week-3/src/app/app.module.ts
+++ week-3/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
-import { OutputComponent } from './output/output.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InputComponent} from './input/input.component';
+import {OutputComponent} from './output/output.component';
 
 @NgModule({
   declarations: [
